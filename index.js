267b004cc085fe42e59fd671a6032c20d383e7d3
Renslr <rendraramadan13@gmail.com>
2023-10-19 09:37:25 +0700

    Final

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const data_users = [
   { id: 2, name: "Giya Saputri", alamat: "Dungsema" },
   { id: 3, name: "Stefany Stellar", alamat: "Wakanda" },
   { id: 4, name: "Iyan maulana", alamat: "Kembar" },
+  { id: 5, name: "Faris Ahmad Suki", alamat: "Logam" },
 ];
 
 app.get("/pengguna", (req, res) => {
@@ -15,6 +16,36 @@ app.get("/pengguna", (req, res) => {
       data: data,
   };
 
+  res.json(result);
+});
+
+
+app.get("/pengguna/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  let result;
+  const user = data_users.find((user) => user.id === id);
+  if (user) {
+      result = {
+      status: 200,
+      data: user,
+      };
+  }
+  res.json(result);
+});
+
+
+app.get("/pengguna/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  let result;
+  const user = data_users.find((user) => user.id === id);
+  if (user) {
+      result = {
+      status: 200,
+      data: user,
+      };
+  } else {
+      res.status(404).json({ error: "User not found" });
+  }
   res.json(result);
 });
 app.listen(port,() => console.log(`Server running on port ${port}`))
